4fcca8b1101c4d476189242c3f18fdf13e015a1d
minjaekim <minjae2246@gmail.com>
2022-01-02 16:13:32 +0900

    5_2.자주 쓰이는 미들웨어 정리완료

--- 5장_익스프레스로 웹서버 만들기/실습 파일/app.js
+++ 5장_익스프레스로 웹서버 만들기/실습 파일/app.js
@@ -6,10 +6,70 @@ const app = express();
 app.set("port", process.env.PORT || 3000);
 // 서버에 속성 심는 것, 포트 속성 3000을 넣어서 설정
 
+// 공통되는 코드를 빼줄 수 있다. 모든 라우터(메서드와 주소)에 요청을 보냄
+app.use(
+  (req, res, next) => {
+    console.log(" 1 모든 요청에 실행");
+    next();
+  }
+  // (req, res, next) => {
+  //   console.log("2 모든 요청에 실행");
+  //   next();
+  // },
+  // (req, res, next) => {
+  //   try {
+  //     console.log(asdfasdf);
+  //   } catch (error) {
+  //     next(error);
+  //   }
+  // }
+);
+
+app.get(
+  "/",
+  (req, res, next) => {
+    // res.send("hi express") - 단순 문자열로 응답;
+    // HTML로 응답
+    res.sendFile(path.join(__dirname, "index.html"));
+    if (true) {
+      next("route");
+    } else {
+      next();
+    }
+  },
+  (req, res) => {
+    res.send(`false면 실행`);
+  }
+);
+
 app.get("/", (req, res) => {
-  // res.send("hi express") - 단순 문자열로 응답;
-  res.sendFile(path.join(__dirname, "index.html"));
-  // HTML로 응답
+  res.send(`true면 실행!`);
+});
+
+app.post("/", (req, res) => {
+  res.send("Hello express");
+});
+
+app.get("/category/:name", (req, res) => {
+  res.send(`hi ${req.params.name}`);
+});
+
+// app.get("*", (req, res) => {
+//   res.send(`hi ${req.params.name}`);
+// });
+
+app.get("/about", (req, res) => {
+  res.send("Hi express");
+});
+
+app.use((req, res, next) => {
+  res.status(404).send("404");
+});
+
+// 에러 처리 미들웨어로 모든 에러를 처리해준다.
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.send("error happen");
 });
 
 app.listen(app.get("port"), () => {
